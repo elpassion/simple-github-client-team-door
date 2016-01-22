49c6d6b91c3f9a80e3ddf1625f379aaaff2ed810
rafal <trzcinskiraf@gmail.com>
2016-01-22 10:57:41 +0100

    Add crashlytics

--- app/src/main/java/pl/elpassion/door/githubclient/GithubSearchViewActivity.kt
+++ app/src/main/java/pl/elpassion/door/githubclient/GithubSearchViewActivity.kt
@@ -1,19 +1,19 @@
 package pl.elpassion.door.githubclient
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.View
 import android.view.Menu
-import android.view.MenuItem
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class GithubSearchViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.github_search_view)
+        Fabric.with(this, Crashlytics());
+
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
