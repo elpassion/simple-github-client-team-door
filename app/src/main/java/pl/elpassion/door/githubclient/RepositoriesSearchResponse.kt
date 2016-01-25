f320704d47b56349eedd2889531b909a2da1e542
dmalantowicz <dominik.malantowicz@elpassion.pl>
2016-01-25 15:48:12 +0100

    refactoring

--- app/src/main/java/pl/elpassion/door/githubclient/RepositoriesSearchResponse.kt
+++ app/src/main/java/pl/elpassion/door/githubclient/RepositoriesSearchResponse.kt
@@ -1,7 +1,4 @@
 package pl.elpassion.door.githubclient
 
 class RepositoriesSearchResponse(val items : List<Repository>)
-class Repository (override val name : String) : GithubSearchItem {
-
-}
-
+class Repository (override val name : String) : GithubSearchItem
