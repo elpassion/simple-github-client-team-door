751a301bbf58110dfa803ce1c487b2803f768cb1
dmalantowicz <dominik.malantowicz@elpassion.pl>
2016-01-25 10:29:26 +0100

    Fix to Gson mapping

--- app/src/main/java/pl/elpassion/door/githubclient/UserSearchResponse.kt
+++ app/src/main/java/pl/elpassion/door/githubclient/UserSearchResponse.kt
@@ -1,4 +1,6 @@
 package pl.elpassion.door.githubclient
 
+import com.google.gson.annotations.SerializedName
+
 class UserSearchResponse(val items: List<User>)
-class User(override val name: String, val avatarUrl: String, val reposUrl: String) : GithubSearchItem
+class User(@SerializedName(value="login") override val name: String, val avatarUrl: String, val reposUrl: String) : GithubSearchItem
