1d2277faf20bd8fac2aa5fb2477dee4c0c02ea0b
dmalantowicz <dominik.malantowicz@elpassion.pl>
2016-01-25 14:13:47 +0100

    added Parcelable to User class

--- app/src/main/java/pl/elpassion/door/githubclient/UserSearchResponse.kt
+++ app/src/main/java/pl/elpassion/door/githubclient/UserSearchResponse.kt
@@ -1,6 +1,35 @@
 package pl.elpassion.door.githubclient
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 class UserSearchResponse(val items: List<User>)
-class User(@SerializedName(value="login") override val name: String, val avatarUrl: String, val reposUrl: String) : GithubSearchItem
+class User(@SerializedName(value="login") override val name: String, val avatarUrl: String, val reposUrl: String) : GithubSearchItem, Parcelable {
+
+    companion object{
+        final val CREATOR : Parcelable.Creator<User> = object : Parcelable.Creator<User> {
+
+            override fun createFromParcel(parcel : Parcel) : User{
+                return User(parcel)
+            }
+
+            override fun newArray(size : Int) : Array<User?> {
+                return arrayOfNulls<User>(size)
+            }
+        }
+    }
+
+    constructor(parcel: Parcel) : this(parcel.readString(), parcel.readString(), parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags : Int) {
+        parcel.writeString(name)
+        parcel.writeString(avatarUrl)
+        parcel.writeString(reposUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+}
