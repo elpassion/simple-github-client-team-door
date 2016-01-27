3c4b8868dc23aa4dae2b520049b2e4a4fa777196
rafal <trzcinskiraf@gmail.com>
2016-01-27 10:40:28 +0100

    fix to test and travis.yml

--- app/src/androidTest/java/pl/elpassion/door/githubclient/ApplicationTest.kt
+++ app/src/androidTest/java/pl/elpassion/door/githubclient/ApplicationTest.kt
@@ -3,7 +3,8 @@ package pl.elpassion.door.githubclient
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.test.ActivityInstrumentationTestCase2
 import pl.elpassion.door.githubclient.activity.GithubSearchViewActivity
 import pl.elpassion.door.githubclient.service.github.GithubService
@@ -13,18 +14,27 @@ import rx.Observable
 
 
 class ApplicationTest : ActivityInstrumentationTestCase2<GithubSearchViewActivity>(GithubSearchViewActivity::class.java) {
+
     override fun setUp() {
         super.setUp()
+        activity
+    }
+
+    private fun typeTextToSearch(text : String) {
+        onView(withId(R.id.search_name)).perform(typeText(text))
+    }
+
+
+    fun testIfSnackBarShowsWhenNoGithubConnection() {
+
         GithubService.githubUsersService = object : GithubUsersService {
             override fun searchForUsersByName(name: String): Observable<UserSearchResponse> {
                 return Observable.error(Throwable())
             }
         }
-        activity
+
+        typeTextToSearch("tom")
+        onView(withId(android.support.design.R.id.snackbar_text)).check(matches(isDisplayed()))
     }
 
-    fun testIfSnackBarShowsWhenNoInternetConnection() {
-        onView(withId(R.id.search_name)).perform(typeText("tom"))
-        onView(withText("Problem z pobraniem danych z Githuba")).check(matches(isDisplayed()))
-    }
 }
